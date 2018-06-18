86a8597c16458f7300b48f74373be89f8a693c13
SCOPUS\dgarcia <dgarcia@scopus.com.br>
2018-06-18 09:51:26 -0300

    Extrato

--- messenger-banking/src/main/java/br/com/scopus/bot/messenger/banking/FbBot.java
+++ messenger-banking/src/main/java/br/com/scopus/bot/messenger/banking/FbBot.java
@@ -1,6 +1,7 @@
 package br.com.scopus.bot.messenger.banking;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import br.com.scopus.messenger.core.Bot;
 import br.com.scopus.messenger.core.common.Controller;
@@ -18,6 +19,7 @@ import br.com.scopus.messenger.core.models.Payload;
  *
  */
 @IBot
+@Service
 public class FbBot extends Bot {
 
     /**
@@ -42,66 +44,76 @@ public class FbBot extends Bot {
         return pageAccessToken;
     }
 
+    @Value("${text.messenger.extrato}")
+    private String msgExtrato;
+
     /**
-     * Sets the "Get Started" button with a payload "hi". It also set the "Greeting Text" which the user sees when it
-     * opens up the chat window. Uncomment the {@code @PostConstruct} annotation only after you have verified your 
-     * webhook.
+     * Sets the "Get Started" button with a payload "hi". It also set the "Greeting Text" which the
+     * user sees when it opens up the chat window. Uncomment the {@code @PostConstruct} annotation
+     * only after you have verified your webhook.
      */
-    //@PostConstruct
+    // @PostConstruct
     public void init() {
         setGetStartedButton("hi");
-        setGreetingText(new Payload[]{new Payload().setLocale("default").setText("JBot is a Java Framework to help" +
-                " developers make Facebook, Slack and Twitter bots easily. You can see a quick demo by clicking " +
-                "the \"Get Started\" button.")});
+        setGreetingText(new Payload[] { new Payload().setLocale("default")
+            .setText("JBot is a Java Framework to help"
+                + " developers make Facebook, Slack and Twitter bots easily. You can see a quick demo by clicking "
+                + "the \"Get Started\" button.") });
     }
 
     /**
-     * This method gets invoked when a user clicks on the "Get Started" button or just when someone simply types
-     * hi, hello or hey. When it is the former, the event type is {@code EventType.POSTBACK} with the payload "hi"
-     * and when latter, the event type is {@code EventType.MESSAGE}.
+     * This method gets invoked when a user clicks on the "Get Started" button or just when someone
+     * simply types hi, hello or hey. When it is the former, the event type is
+     * {@code EventType.POSTBACK} with the payload "hi" and when latter, the event type is
+     * {@code EventType.MESSAGE}.
      *
      * @param event
      */
     @Controller(events = { EventType.MESSAGE }, pattern = "^(?i)(ola|olá|oi|como vai|ops)$")
     public void onGetStarted(Event event) {
-        
+
         // quick reply buttons
-        Button[] quickReplies = new Button[]{
-                new Button().setContentType("text").setTitle("Saldo").setPayload("Saldo"),
-                new Button().setContentType("text").setTitle("Extrato").setPayload("Extrato"),
-                new Button().setContentType("text").setTitle("Recarga").setPayload("Recarga")
-        };
+        Button[] quickReplies = new Button[] {
+            new Button().setContentType("text").setTitle("Saldo").setPayload("Saldo"),
+            new Button().setContentType("text").setTitle("Extrato").setPayload("Extrato"),
+            new Button().setContentType("text").setTitle("Recarga").setPayload("Recarga") };
         reply(event, new Message().setText("Em que posso ajudar?").setQuickReplies(quickReplies));
     }
 
     /**
-     * This method gets invoked when the user clicks on a quick reply button whose payload is either "yes" or "no".
-     *
+     * Este metodo e invocado quando o usario utiliza o botao de "Extrato".
+     * 
      * @param event
      */
-    @Controller(events = EventType.QUICK_REPLY, pattern = "(yes|no)")
-    public void onReceiveQuickReply(Event event) {
-        if ("yes".equals(event.getMessage().getQuickReply().getPayload())) {
-            reply(event, "Cool! You can type: \n 1) Show Buttons \n 2) Show List \n 3) Setup meeting");
-        } else {
-            reply(event, "See you soon!");
-        }
+    @Controller(events = EventType.QUICK_REPLY, pattern = "Extrato")
+    public void replyExtatoReply(Event event) {
+        reply(event, this.getMsgExtrato());
     }
 
     /**
-     * This method is invoked when the user types "Show Buttons" or something which has "button" in it as defined
-     * in the {@code pattern}.
+     * Este metodo e invocado quando o usario envia as palavras(Extrato|extrato|estrato) o botao de
+     * "Extrato".
+     * 
+     * @param event
+     */
+    @Controller(events = EventType.MESSAGE, pattern = "(Extrato|extrato|estrato|Estrato)")
+    public void replyExtatoMessage(Event event) {
+        reply(event, this.getMsgExtrato());
+    }
+
+    /**
+     * This method is invoked when the user types "Show Buttons" or something which has "button" in
+     * it as defined in the {@code pattern}.
      *
      * @param event
      */
     @Controller(events = EventType.MESSAGE, pattern = "(?i:button)")
     public void showButtons(Event event) {
-        Button[] buttons = new Button[]{
-                new Button().setType("web_url").setUrl("http://blog.ramswaroop.me").setTitle("JBot Docs"),
-                new Button().setType("web_url").setUrl("https://goo.gl/uKrJWX").setTitle("Buttom Template")
-        };
-        reply(event, new Message().setAttachment(new Attachment().setType("template").setPayload(new Payload()
-                .setTemplateType("button").setText("These are 2 link buttons.").setButtons(buttons))));
+        Button[] buttons = new Button[] {
+            new Button().setType("web_url").setUrl("http://blog.ramswaroop.me").setTitle("JBot Docs"),
+            new Button().setType("web_url").setUrl("https://goo.gl/uKrJWX").setTitle("Buttom Template") };
+        reply(event, new Message().setAttachment(new Attachment().setType("template").setPayload(
+            new Payload().setTemplateType("button").setText("These are 2 link buttons.").setButtons(buttons))));
     }
 
     /**
@@ -111,44 +123,46 @@ public class FbBot extends Bot {
      */
     @Controller(events = EventType.MESSAGE, pattern = "(?i)(bye|tchau|fui|falou|flw|até|até)")
     public void showGithubLink(Event event) {
-        reply(event, new Message().setAttachment(new Attachment().setType("template").setPayload(new Payload()
-                .setTemplateType("button").setText("Bye. Happy coding!").setButtons(new Button[]{new Button()
-                        .setType("web_url").setTitle("View code").setUrl("https://github.com/ramswaroop/jbot")}))));
+        reply(event,
+            new Message().setAttachment(new Attachment().setType("template")
+                .setPayload(new Payload().setTemplateType("button").setText("Bye. Happy coding!")
+                    .setButtons(new Button[] { new Button().setType("web_url").setTitle("View code")
+                        .setUrl("https://github.com/ramswaroop/jbot") }))));
     }
 
 
     // Conversation feature of JBot
 
     /**
-     * Type "setup meeting" to start a conversation with the bot. Provide the name of the next method to be
-     * invoked in {@code next}. This method is the starting point of the conversation (as it
-     * calls {@link IBot#startConversation(Event, String)} within it. You can chain methods which will be invoked
-     * one after the other leading to a conversation.
+     * Type "setup meeting" to start a conversation with the bot. Provide the name of the next
+     * method to be invoked in {@code next}. This method is the starting point of the conversation
+     * (as it calls {@link IBot#startConversation(Event, String)} within it. You can chain methods
+     * which will be invoked one after the other leading to a conversation.
      *
      * @param event
      */
     @Controller(pattern = "(?i)(setup meeting)", next = "confirmTiming")
     public void setupMeeting(Event event) {
-        startConversation(event, "confirmTiming");   // start conversation
+        startConversation(event, "confirmTiming"); // start conversation
         reply(event, "Cool! At what time (ex. 15:30) do you want me to set up the meeting?");
     }
 
     /**
-     * This method will be invoked after {@link FbBot#setupMeeting(Event)}. You need to
-     * call {@link IBot#nextConversation(Event)} to jump to the next question in the conversation.
+     * This method will be invoked after {@link FbBot#setupMeeting(Event)}. You need to call
+     * {@link IBot#nextConversation(Event)} to jump to the next question in the conversation.
      *
      * @param event
      */
     @Controller(next = "askTimeForMeeting")
     public void confirmTiming(Event event) {
-        reply(event, "Your meeting is set at " + event.getMessage().getText() +
-                ". Would you like to repeat it tomorrow?");
-        nextConversation(event);    // jump to next question in conversation
+        reply(event,
+            "Your meeting is set at " + event.getMessage().getText() + ". Would you like to repeat it tomorrow?");
+        nextConversation(event); // jump to next question in conversation
     }
 
     /**
-     * This method will be invoked after {@link FbBot#confirmTiming(Event)}. You can
-     * call {@link IBot#stopConversation(Event)} to end the conversation.
+     * This method will be invoked after {@link FbBot#confirmTiming(Event)}. You can call
+     * {@link IBot#stopConversation(Event)} to end the conversation.
      *
      * @param event
      */
@@ -156,16 +170,16 @@ public class FbBot extends Bot {
     public void askTimeForMeeting(Event event) {
         if (event.getMessage().getText().contains("yes")) {
             reply(event, "Okay. Would you like me to set a reminder for you?");
-            nextConversation(event);    // jump to next question in conversation  
+            nextConversation(event); // jump to next question in conversation
         } else {
             reply(event, "No problem. You can always schedule one with 'setup meeting' command.");
-            stopConversation(event);    // stop conversation only if user says no
+            stopConversation(event); // stop conversation only if user says no
         }
     }
 
     /**
-     * This method will be invoked after {@link FbBot#askTimeForMeeting(Event)}. You can
-     * call {@link IBot#stopConversation(Event)} to end the conversation.
+     * This method will be invoked after {@link FbBot#askTimeForMeeting(Event)}. You can call
+     * {@link IBot#stopConversation(Event)} to end the conversation.
      *
      * @param event
      */
@@ -176,6 +190,14 @@ public class FbBot extends Bot {
         } else {
             reply(event, "Okay, don't forget to attend the meeting tomorrow :)");
         }
-        stopConversation(event);    // stop conversation
+        stopConversation(event); // stop conversation
+    }
+
+    public String getMsgExtrato() {
+        return msgExtrato;
+    }
+
+    public void setMsgExtrato(String msgExtrato) {
+        this.msgExtrato = msgExtrato;
     }
 }
